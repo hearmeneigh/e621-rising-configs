3fdd905fd860e47ab2183efaf1f9477a7619484d
Mr. Stallion <mrstallion@nobody.nowhere.fauxdomain.ext>
2023-10-17 14:45:38 -0700

    minor

--- util/trainer.sh
+++ util/trainer.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -ex
 
+# This script can be used to run the training process for E621 Rising.
+# It is configured for 1x Nvidia A100 80GB GPU
+
 export DATASET="hearmeneigh/e621-rising-v3-curated"  # dataset to train on
+export OPTIMIZED_DATASET="/workspace/cache/optimize/hearmeneigh/e621-rising-v3-curated"  # <-- use this after the first epoch (saves ~24h/epoch)
+
 export BASE_MODEL="stabilityai/stable-diffusion-xl-base-1.0"  # model to start from
 export BASE_PATH="/workspace"
 
-export MODEL_NAME="hearmeneigh/e621-rising-v3"  # Huggingface name of the model we're training/finetuning from
+export MODEL_NAME="hearmeneigh/e621-rising-v3"  # Huggingface name of the model we're training/finetuning
 export RESOLUTION=1024
 export EPOCHS_PER_ITERATION=1
 export PRECISION=fp16
@@ -14,8 +19,15 @@ export CACHE_PATH="${BASE_PATH}/cache"
 
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
+
+# Override Huggingface cache paths
+export XDG_CACHE_HOME=
+unset XDG_CACHE_HOME
+export HF_HOME="${BASE_PATH}/cache/huggingface"
 export HF_DATASETS_CACHE="${BASE_PATH}/cache/huggingface/datasets"
 export HF_MODULES_CACHE="${BASE_PATH}/cache/huggingface/modules"
+export HUGGINGFACE_HUB_CACHE="${BASE_PATH}/cache/huggingface/hub"
+export HUGGINGFACE_ASSETS_CACHE="${BASE_PATH}/cache/huggingface/assets"
 
 export MAX_EPOCHS=100
 
@@ -31,7 +43,7 @@ fi
 
 if [ -z "${AWS_BASE_PATH}" ]
 then
-  AWS_BASE_PATH='s3://sd-hmn/v3-sdxl-step-'
+  AWS_BASE_PATH='s3://sd-hmn/e621-rising-v3-epoch-'
 fi
 
 if [ -z "${TRAINER_FILE}" ]
@@ -55,6 +67,7 @@ do
   else
     PREV_EPOCH=$(($CUR_EPOCH-1))
     export MODEL="${OUTPUT_BASE_PATH}${PREV_EPOCH}"
+    export DATASET="${OPTIMIZED_DATASET}"
     export RESUME_ARG='--resume-from-checkpoint=latest'
   fi
 
@@ -84,15 +97,10 @@ do
     --reshuffle-tags \
     --gradient-checkpointing \
     --gradient-accumulation-steps=1 \
-    --force-temp-dataset-store-dir="${CACHE_PATH}/optimize/${DATASET}" \
+    --force-temp-dataset-store-dir="${OPTIMIZED_DATASET}" \
     --fingerprint-batch-size=20 \
     --checkpointing-steps=3500 \
     ${RESUME_ARG}
-
-
-  # batch_size=1
-  # accumulation steps could be ~ 8-10?
-  # does that mean batch_size could be 8-10 if accumulation = 1?
 
   rm -rf ${OUTPUT_PATH}/checkpoint-*
 
